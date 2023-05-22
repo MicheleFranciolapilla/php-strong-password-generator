02989a2db296dd15da3a93e2f6075b8124919837
MicheleFranciolapilla <michele.franciolapilla@gmail.com>
2023-05-23 00:05:13 +0900

    array_keys per entrambi gli array associativi

--- index.php
+++ index.php
@@ -15,8 +15,8 @@ Dare all’utente anche la possibilità di permettere o meno la ripetizione di c
 <?php
     // Array con valori booleani indicanti le categorie di caratteri (sub array in $char_set_array) da utilizzare per la composizione della password
     $parameters = [
-        "letters" => false,
-        "numbers" => false,
+        "letters" => true,
+        "numbers" => true,
         "symbols" => true
     ];
 
@@ -26,20 +26,22 @@ Dare all’utente anche la possibilità di permettere o meno la ripetizione di c
 
     // Array multidimensionale con tre sotto-array contenenti, nel complesso, la totalità dei caratteri validi per la composizione della password
     $char_set_array = [
-        ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'],
-        ['0','1','2','3','4','5','6','7','8','9'],
-        ['!','?','#','$','%','&','@']
+        "letters" => ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'],
+        "numbers" => ['0','1','2','3','4','5','6','7','8','9'],
+        "symbols" => ['!','?','#','$','%','&','@']
     ];
 
+    // Funzione che crea l'array con gli effettivi caratteri validi con i quali comporre la password
     function create_valid_char_array($parameters, $char_set_array)
     {
         $final_array = [];
+        // L'array $keys consentirà di puntare direttamente alla chiave booleana (in $parameters) ed alla chiave array (in $char_set_array) trattandole come chiavi numeriche, pur essendo chiavi di array associativi. In questo modo si mantiene una chiarezza semantica e di immediato riconoscimento
         $keys = array_keys($parameters);
         for ($i = 0; $i < count($parameters); $i++)
         {
             if ($parameters[$keys[$i]])
             {
-                foreach($char_set_array[$i] as $char)
+                foreach($char_set_array[$keys[$i]] as $char)
                 {
                     $final_array[] = $char;
                 }
@@ -48,6 +50,7 @@ Dare all’utente anche la possibilità di permettere o meno la ripetizione di c
         return $final_array;
     }
 
+    
     $valid_char_array = create_valid_char_array($parameters, $char_set_array);
     var_dump("Array appena generato: ",$valid_char_array);
 
