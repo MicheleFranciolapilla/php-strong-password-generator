4414e0b119d38b82da2655d204d91f8992ad322b
MicheleFranciolapilla <michele.franciolapilla@gmail.com>
2023-05-23 10:15:49 +0900

    Milestone 1: ok

--- index.php
+++ index.php
@@ -20,16 +20,15 @@ Dare all’utente anche la possibilità di permettere o meno la ripetizione di c
         "symbols" => true
     ];
 
-    // $letters = true;
-    // $numbers = true;
-    // $symbols = true;
-
     // Array multidimensionale con tre sotto-array contenenti, nel complesso, la totalità dei caratteri validi per la composizione della password
     $char_set_array = [
         "letters" => ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'],
         "numbers" => ['0','1','2','3','4','5','6','7','8','9'],
         "symbols" => ['!','?','#','$','%','&','@']
     ];
+
+    // Valore provvisorio. Questa variabile assumerà il valore booleano stabilito nel form (bonus)
+    $allow_repeated = true;
 
     // Funzione che crea l'array con gli effettivi caratteri validi con i quali comporre la password
     function create_valid_char_array($parameters, $char_set_array)
@@ -49,27 +48,45 @@ Dare all’utente anche la possibilità di permettere o meno la ripetizione di c
         }
         return $final_array;
     }
-
     
+    // Creazione effettiva dell'array con i caratteri validi per la generazione della password
     $valid_char_array = create_valid_char_array($parameters, $char_set_array);
-    var_dump("Array appena generato: ",$valid_char_array);
 
-    function generate_psw($password_length)
+    function generate_psw($password_length, $valid_char_array, $letters_array, $allow_repeated)
     {
+        // RICORDARSI DI INSERIRE UNA LOGICA CHE TENGA CONTO DELLA QUANTITA' DEI CARATTERI VALIDI E DEL PASSWORD LENGTH
         $psw_array = [];
-        var_dump("Array vuoto: ",$psw_array);
+        $index = 0;
         for ($i = 0; $i < $password_length; $i++)
         {
+            $index = mt_rand(0,count($valid_char_array) - 1);
+            $char = $valid_char_array[$index];
+            // echo "Indice di creazione password: " . $i . "<br>";
+            // echo "Carattere generato: " . $char . "<br>";
+            if (array_search($char, $letters_array, true) !== false)
+            {
+                // significa che si tratta di una lettera e dunque si deve eseguire un random booleano per decidere se deve essere maiuscola o minuscola
+                if (boolval(mt_rand(0,1)))
+                    $char = strtoupper($char);
+            }
+            // Se il carattere $char generato e pronto per essere inserito nella password è già presente (strettamente) e non sono permesse ripetizioni di carattere, allora si decrementa l'indice e si ripete il ciclo, altrimenti il carattere viene definitivamente inserito nella password
+            if (!$allow_repeated && array_search($char, $psw_array, true) !== false)
+                $i--;
+            else
+                $psw_array[] = $char;
+                // echo "Array della password: " . implode("",$psw_array) . "<br>";
         }
+        return implode("",$psw_array);
     };
 
+    $psw_length = 0;
+    $password = "";
 
     if (isset($_GET['psw_length']))
     {
         $psw_length = $_GET['psw_length'];
-        $password = generate_psw($psw_length);
+        $password = generate_psw($psw_length, $valid_char_array, $char_set_array["letters"], $allow_repeated);
     }
-    var_dump("password length = ", $psw_length);
 ?>
 
 <!DOCTYPE html>
@@ -97,6 +114,17 @@ Dare all’utente anche la possibilità di permettere o meno la ripetizione di c
                 <button class="btn btn-primary" type="submit">Conferma</button>
             </form>
         </section>
+        <?php
+            if (isset($_GET['psw_length'])) :
+        ?>
+        <section id="output_section" class="my-5">
+            <?php
+                echo "<span>La password generata è la seguente: " . $password . "</span>";
+            ?>
+        </section>
+        <?php
+            endif;
+        ?>
     </main>
     
 <!-- CDN per Bootstrap 5 -->
